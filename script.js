45b2c13afd294a4b5626dbd5c5f6e19b626636bc
Luke <116241757+Nyrexis@users.noreply.github.com>
2022-12-15 18:17:46 +1100

    day/clock

--- script.js
+++ script.js
@@ -1,27 +1,60 @@
-var saveBtn = document.querySelector(".fas")
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+var saveBtn = $(".saveBtn")
+var textBox = $(".description")
+var Tblock = $(".time-block")
+var storage = ""
+var time = "0"
+var date;
+var chosen;
+var usertext;
+
+
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  $( textBox ).each( function(){
+    var contentId = $(this).parent().attr('id')
+    var newcontent = localStorage.getItem(contentId)
+    console.log(newcontent)
+    if (newcontent !== null) {
+      $(this).val(newcontent)
+    }
+  });
+  console.log(textBox)
+  var today = dayjs();
+  setInterval(function() { 
+    today = dayjs();
+    $('#currentDay').text(today.format('dddd, MMMM D YYYY, h:mm:ss a'));
+ 
+  $( Tblock ).each( function(){
+if ($(this).attr("data-time") < today.hour()){
+    $(this).addClass("past");
+    $(this).removeClass("present future");
+ } else if ($(this).attr("data-time") == today.hour()){
+   $(this).addClass("present")
+   $(this).removeClass("future past")
+ } else if ($(this).attr("data-time") > today.hour()){
+   $(this).addClass("future")
+   $(this).removeClass("present past")
+ }
 });
-saveBtn.addEventListener("click", function(){
-  console.log("works")
-})
+  }, 1000);
+
+
+
+});
+function savefn(){
+  console.log($(chosen).prev());
+  time = ($(chosen).prev());
+  console.log($(time).val())
+  console.log($(time).parent().attr('id'))
+  var timeslot = ($(time).parent().attr('id'))
+  localStorage.setItem(timeslot, $(time).val());
+}
+$(saveBtn).on('click', function (event) {
+  chosen = event.target;
+  $(savefn)
+});
+
+$(textBox).on('input', function handleChange(event) {
+  console.log(event.target.value);
+  usertext = event.target.value
+  event.target.textContent = usertext.trim()
+});
